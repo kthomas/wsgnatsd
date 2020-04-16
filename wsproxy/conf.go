01ba07e51d7524d99a3834fdc91f68b6b6950c69
Kyle Thomas <kyle@provide.services>
2020-04-16 12:39:25 -0400

    Support ws-port flag

--- wsproxy/conf.go
+++ wsproxy/conf.go
@@ -22,6 +22,7 @@ type Opts struct {
 	TextFrames         bool
 	Trace              bool
 	WSHostPort         string
+	WSPort             string
 
 	// when true, the authorization http header provided to the websocket request in the form `bearer: <jwt>` is implicitly used to send a CONNECT message to NATS upon successful connection
 	WSRequireAuthorization bool
@@ -108,6 +109,12 @@ func ParseOpts(c string) (*Opts, error) {
 				panic(err)
 			}
 			o.WSHostPort = v
+		case "ws-port":
+			v, err := parseStringValue(k, v)
+			if err != nil {
+				panic(err)
+			}
+			o.WSPort = v
 		case "ws-require-authorization":
 			v, err := parseBooleanValue(k, v)
 			if err != nil {
--- wsproxy/server.go
+++ wsproxy/server.go
@@ -136,6 +136,7 @@ func parseFlags(embedded bool) (*Opts, error) {
 	//opts.StringVar(&confFile, "c", "", "configuration file")
 	opts.StringVar(&c.Auth, "auth", c.Auth, "NATS auth token")
 	opts.StringVar(&c.WSHostPort, "h", c.WSHostPort, "ws-host - default is 0.0.0.0:4221")
+	opts.StringVar(&c.WSPort, "p", c.WSPort, "ws-port - default is 4221")
 	opts.BoolVar(&c.WSRequireAuthorization, "a", c.WSRequireAuthorization, "ws-require-authorization - when true, the authorization http header provided to the websocket request in the form `bearer: <jwt>` is implicitly used to send a CONNECT message to NATS")
 	opts.BoolVar(&c.WSRequireTLS, "wstls", c.WSRequireTLS, "require-tls - require the use of TLS by generating self-signed certificate")
 	opts.StringVar(&c.RemoteNatsHostPort, "nh", c.RemoteNatsHostPort, "nats-host - disables embedded NATS and proxies requests to the specified hostport")
